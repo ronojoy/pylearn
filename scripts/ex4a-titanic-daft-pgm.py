1ead6c95972435419147c945521531107f822feb
Ronojoy Adhikari <ronojoy.adhikari@gmail.com>
2015-06-08 22:17:24 +0530

    cleanups for karthick

--- scripts/ex4a-titanic-daft-pgm.py
+++ scripts/ex4a-titanic-daft-pgm.py
@@ -4,13 +4,27 @@ rc("text", usetex=True)
 
 import daft
 
-pgm = daft.PGM([5,4], origin=[1.15, 0.65])
-pgm.add_node(daft.Node("survived", r"survived", 4, 4, aspect=1.8))
-pgm.add_node(daft.Node("age", r"age", 2, 2, aspect=1.2, observed=True))
-pgm.add_node(daft.Node("gender", r"gender", 3, 2, aspect=2.1, observed=True))
-pgm.add_node(daft.Node("class", r"class", 4.5, 2, aspect=2.4, observed=True))
-pgm.add_node(daft.Node("embark", r"embark", 6, 2, aspect=2.4, observed=True))
-pgm.add_node(daft.Node("name", r"name", 3, 1, aspect=2.4, observed=True))
+# instantiate a pgm object
+pgm = daft.PGM([6,4], origin=[0, 1])
+
+
+# create the nodes
+survived = daft.Node("survived", r"survived", 4, 4, aspect = 1.8)
+age = daft.Node("age", r"age", 2, 3, aspect = 1.8)
+gender = daft.Node("gender", r"gender", 3, 3, aspect = 1.8, observed=True)
+tclass = daft.Node("class", r"class", 4, 3, aspect = 1.8,observed=True)
+embark = daft.Node("embark", r"embark", 5, 3, aspect = 1.8, observed=True)
+name = daft.Node("name", r"name", 3, 2, aspect = 1.8, observed=True)
+
+# add the nodes to pgm object
+pgm.add_node(survived)
+pgm.add_node(age)
+pgm.add_node(gender)
+pgm.add_node(tclass)
+pgm.add_node(embark)
+pgm.add_node(name)
+
+# add edges between nodes
 pgm.add_edge("survived", "age")
 pgm.add_edge("survived", "gender")
 pgm.add_edge("survived", "class")
